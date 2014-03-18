f0d9eb096855f30f54b15e8ad16b17e26d86de89
Alan <alan@foobar.com>
2014-03-18 13:08:05 -0700

    Refactor into separate files and use extension method for decrement

---
+++ Branching/Scenario01/Countdown.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Scenario01
+{
+    class Countdown
+    {
+        private int _value = 0;
+
+        public Countdown(int value)
+        {
+            _value = value;
+        }
+
+        public int Decrement()
+        {
+            return _value = _value.Decrement();
+        }
+    }
+
+}
---
+++ Branching/Scenario01/IntExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario01
+{
+    static class IntExtensions
+    {
+        public static int Decrement(this int value)
+        {
+            return --value;
+        }
+    }
+}
