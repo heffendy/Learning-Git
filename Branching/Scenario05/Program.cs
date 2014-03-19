6d7306ef6290c695bbf62354c1f7c6d41e627cd8
Alan <alan@foobar.com>
2014-03-18 21:59:49 -0700

    Implement Countdown

--- Branching/Scenario05/Program.cs
+++ Branching/Scenario05/Program.cs
@@ -8,9 +8,31 @@ namespace Scenario05
 {
     class Program
     {
+        class Countdown
+        {
+            private int _value = 0;
+
+            public Countdown(int value)
+            {
+                _value = value;
+            }
+
+            public int Decrement()
+            {
+                return --_value;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Countdown countdown = new Countdown(5);
+
+            Console.WriteLine(countdown.Decrement()); // 4
+            Console.WriteLine(countdown.Decrement()); // 3
+            Console.WriteLine(countdown.Decrement()); // 2
+            Console.WriteLine(countdown.Decrement()); // 1
+            Console.WriteLine(countdown.Decrement()); // 0
+
             Console.ReadLine();
         }
     }
